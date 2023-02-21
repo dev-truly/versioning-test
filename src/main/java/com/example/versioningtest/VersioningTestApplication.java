4d149302c0caad03c3d7af6b0c92784081df2601
dev-sincerity <kllliv50@gmail.com>
2023-02-21 18:04:22 +0900

    test

---
+++ src/main/java/com/example/versioningtest/VersioningTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.versioningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersioningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersioningTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/versioningtest/VersioningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versioningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
