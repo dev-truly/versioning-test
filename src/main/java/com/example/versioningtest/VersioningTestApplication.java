cfb9ad824ae532f10775a6e1fd38a372204f38e4
dev-sincerity <kllliv50@gmail.com>
2023-07-04 17:13:06 +0900

    fix: test5

--- src/main/java/com/example/versioningtest/VersioningTestApplication.java
+++ src/main/java/com/example/versioningtest/VersioningTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VersioningTestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("테스트 작업");
 		SpringApplication.run(VersioningTestApplication.class, args);
 	}
 
